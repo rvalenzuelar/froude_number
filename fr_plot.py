3f0f451abe093883be00a2f1212e46b17784a7a3
raul <raul.valenzuela@colorado.edu>
2016-07-13 13:24:58 -0600

    last update

--- fr_plot.py
+++ fr_plot.py
@@ -1,9 +1,21 @@
+
+
 import numpy as np
 import matplotlib.pyplot as plt
-import numpy as np
-import os
+#import os
 import Meteoframes as mf
 import pandas as pd
+import seaborn as sns
+
+from matplotlib import rcParams
+rcParams['xtick.labelsize'] = 15
+rcParams['ytick.labelsize'] = 15
+rcParams['axes.labelsize'] = 15
+rcParams['legend.handletextpad'] = 0.1
+rcParams['mathtext.default'] = 'sf'
+
+#home = os.path.expanduser('~')
+home = '/localdata'
 
 '''
  Case 09 is the only with data available for TTA
@@ -14,95 +26,136 @@ import pandas as pd
 def sin(val):
     return np.sin(np.radians(val))
 
-
 def cos(val):
     return np.cos(np.radians(val))
 
-home = os.path.expanduser('~')
+def sqrt(val):
+    return np.sqrt(val)
 
-minU, maxU = [0, 20]
-minN, maxN = [5e-5, 5e-2]
-U = np.linspace(minU, maxU, 1000)  # [m s-1]
-N = np.linspace(minN, maxN, 1000)  # [s-1]
+def squeeze(val):
+    return np.squeeze(val)
 
-UU, NN = np.meshgrid(U, N)
+def get_bvf(flist=None,stat=None,layer=None):
+    
+    ftxt = home + '/BALLOON/all/{}.tsv'
+    stats = []
+    for name in flist:
+        df = mf.parse_sounding2(ftxt.format(name))
+        bvf = pd.DataFrame(index=df.index, columns=['bvf'])
+        dry_cond = df['RH'] < 90
+        bvf['bvf'][dry_cond] = df['bvf_dry'][dry_cond]
+        bvf['bvf'][~dry_cond] = df['bvf_moist'][~dry_cond]
+        if stat == 'median':
+            stats.append(sqrt(bvf.loc[layer[0]:layer[1]].median()))
+        elif stat == 'mean':        
+            stats.append(sqrt(bvf.loc[layer[0]:layer[1]].mean()))
+        elif stat == 'max':
+            stats.append(sqrt(bvf.loc[layer[0]:layer[1]].max()))
+    return squeeze(np.array(stats))
 
-Fr0500 = UU/(NN*500)
-Fr1000 = UU/(NN*1000)
+def main():
+    minU, maxU = [0, 20]
+    minN, maxN = [5e-5, 5e-2]
+    U = np.linspace(minU, maxU, 1000)  # [m s-1]
+    N = np.linspace(minN, maxN, 1000)  # [s-1]
+    
+    UU, NN = np.meshgrid(U, N)
+    
+    Fr0500 = UU/(NN*500)
+    Fr1000 = UU/(NN*1000)
+    
+    ''' estimates of spd and dir from PPIs and RHIs at range
+    where maximum Doppler is clear; usually it is at
+    the same time than sounding but if not clear then
+    choosed the neares PPI time. If Doppler is absent
+    or not clear within an hour then uses NaN '''
+    ppi_wspd_c09 = np.array([15, 15, 18, 15, 13]+[np.nan]*3)
+    ppi_wdir_c09 = np.array([170, 170, 170, 180, 200]+[np.nan]*3)
+    rhi_wspd_c09 = np.array([10, 10, 15, 10, 10]+[np.nan]*3)
 
-''' estimates of spd and dir from PPIs at range
-where maximum Doppler is clear; usually is at
-the same time than sounding but if not clear then
-choosed the neares PPI time. If Doppler is absent
-or not clear within an hour then uses NaN '''
-ppi_wspd = np.array([15, 15, 18, 15, 13, np.nan, np.nan, np.nan])
-ppi_wdir = np.array([170, 170, 170, 180, 200, np.nan, np.nan, np.nan])
-u_comp = -ppi_wspd*sin(ppi_wdir)
+    ppi_wspd_c13 = np.array([30, 25]+[np.nan]*7)
+    ppi_wdir_c13 = np.array([170, 180]+[np.nan]*7)
+    rhi_wspd_c13 = np.array([25, 25]+[np.nan]*7) #  c13 has only 2 rhis in 
+                                                 #  180 azimuth
+    
+    ''' estimates components and upslope wind '''
+    u_comp = -ppi_wspd_c09*sin(ppi_wdir_c09)
+    v_comp = rhi_wspd_c09
+    Up_c09 = -(u_comp*sin(230)+v_comp*cos(230))
+    u_comp = -ppi_wspd_c13*sin(ppi_wdir_c13)
+    v_comp = rhi_wspd_c13
+    Up_c13 = -(u_comp*sin(230)+v_comp*cos(230))   
+    
+    ''' gets BVFs '''
+    
+    stat='median'
+    sfiles = ['20030121_070405', '20030121_090335', '20030121_105906',
+              '20030121_130221', '20030121_235258', '20030122_040333',
+              '20030122_065918', '20030122_172227']
+    medianc09 = np.empty((2,8))    
+    medianc09[0,:] = get_bvf(flist=sfiles,stat=stat,layer=[0,500])
+    medianc09[1,:] = get_bvf(flist=sfiles,stat=stat,layer=[0,1000])
 
-''' estimates of spd from RHIs '''
-rhi_wspd = np.array([10, 10, 15, 10, 10, np.nan, np.nan, np.nan])
-v_comp = rhi_wspd
+    sfiles = ['20040216_1758','20040216_2043','20040217_1510',
+              '20040217_1658','20040217_1859','20040217_2058',
+              '20040217_2159','20040217_2259','20040217_2359',]
+    medianc13 = np.empty((2,9))    
+    medianc13[0,:] = get_bvf(flist=sfiles,stat=stat,layer=[0,500])
+    medianc13[1,:] = get_bvf(flist=sfiles,stat=stat,layer=[0,1000])
+    
+    with sns.axes_style("white"):
+        fig, ax = plt.subplots(figsize=(6, 6))
+    
+    c = ax.contour(UU, NN, Fr0500, [1.0], colors='k',linestyles=':')
+    plt.clabel(c, fmt='h = 0.5 km', manual=[(15, 0.03)])
+    
+    c = ax.contour(UU, NN, Fr1000, [1.0], colors='k',linestyles='-')
+    plt.clabel(c, fmt='h = 1.0 km', manual=[(15, 0.03)]) 
+    
+    ax.scatter(Up_c09, medianc09[0], marker='+', s=100, color='b',lw=2,
+               label='21-23Jan03 0.5 km'.format(stat))
+    
+    ax.scatter(Up_c13, medianc13[0], marker='x', s=100, color='b',lw=2,
+                label='16-18Feb04 0.5 km'.format(stat))
+    
+    ax.scatter(Up_c09, medianc09[1], marker='+', s=100, color='g',lw=2,
+               label='21-23Jan03 1.0 km'.format(stat))  
+    
+    ax.scatter(Up_c13, medianc13[1], marker='x', s=100, color='g',lw=2,
+                label='16-18Feb04 1.0 km'.format(stat))
 
-''' estimate upslope wind '''
-Up = -(u_comp*sin(230)+v_comp*cos(230))
+    ax.annotate("",
+                xy=(0.64, 0.67), xycoords='figure fraction',
+                xytext=(0.74, 0.62), textcoords='figure fraction',
+                arrowprops=dict(arrowstyle="->",
+                                connectionstyle="arc3,rad=0.3",
+                                lw=2),
+                )
+    ax.text(0.6,0.66,'blocked',transform=ax.transAxes,
+            ha='right',fontsize=15)
+    ax.annotate("",
+                xy=(0.72, 0.46), xycoords='figure fraction',
+                xytext=(0.75, 0.58), textcoords='figure fraction',
+                arrowprops=dict(arrowstyle="->",
+                                connectionstyle="arc3,rad=-0.3",
+                                lw=2),
+                )
+    ax.text(0.72,0.41,'unblocked',transform=ax.transAxes,
+            ha='center',va='top',fontsize=15)
 
-''' gets BVFs '''
-sfiles = ['21_070405', '21_090335', '21_105906',
-          '21_130221', '21_235258', '22_040333',
-          '22_065918', '22_172227']
-ftxt = home + '/BALLOON/case09/FRS_200301{}.tsv'
+    ax.set_xlim([minU, maxU])
+    ax.set_ylim([minN, maxN])
+    
+    plt.legend(loc=2, scatterpoints=1,fontsize=15)
+    plt.xlabel(r'U [$m s^{-1}$]')
+    plt.ylabel(r'N [$s^{-1}$]')
+    plt.title('Froude number (Fr=U/Nh)',fontsize=15)
+    
+#    plt.show()
+    
+    fname='/home/raul/Desktop/froude_number_{}.png'.format(stat)
+    plt.savefig(fname, dpi=300, format='png',papertype='letter',
+                bbox_inches='tight')
 
-median500 = []
-mean500 = []
-max500 = []
-median1000 = []
-mean1000 = []
-max1000 = []
-for name in sfiles:
-    df = mf.parse_sounding2(ftxt.format(name))
-    bvf = pd.DataFrame(index=df.index, columns=['bvf'])
-    dry_cond = df['RH'] < 90
-    bvf['bvf'][dry_cond] = df['bvf_dry'][dry_cond]
-    bvf['bvf'][~dry_cond] = df['bvf_moist'][~dry_cond]
 
-    median500.append(np.sqrt(bvf.loc[0:500].median()))
-    mean500.append(np.sqrt(bvf.loc[0:500].mean()))
-    max500.append(np.sqrt(bvf.loc[0:500].max()))
-    median1000.append(np.sqrt(bvf.loc[0:1000].median()))
-    mean1000.append(np.sqrt(bvf.loc[0:1000].mean()))
-    max1000.append(np.sqrt(bvf.loc[0:1000].max()))
-
-median500 = np.squeeze(np.array(median500))
-mean500 = np.squeeze(np.array(mean500))
-max500 = np.squeeze(np.array(max500))
-median1000 = np.squeeze(np.array(median1000))
-mean1000 = np.squeeze(np.array(mean1000))
-max1000 = np.squeeze(np.array(max1000))
-
-fig, ax = plt.subplots(figsize=(8, 7))
-c = ax.contour(UU, NN, Fr0500, [1.0], colors='b')
-plt.clabel(c, fmt='H = 0.5 km', manual=[(15, 0.03)])
-c = ax.contour(UU, NN, Fr1000, [1.0], colors='g')
-plt.clabel(c, fmt='H =1.0 km', manual=[(15, 0.03)])
-ax.scatter(Up, mean500, marker='o', s=100, color='b',
-           facecolor='none', label='mean 0.5 km')
-ax.scatter(Up, mean1000, marker='o', s=100, color='g',
-           facecolor='none', label='mean 1.0 km')
-ax.scatter(Up, median500, marker='+', s=100, color='b',
-           label='median 0.5 km')
-ax.scatter(Up, median1000, marker='+', s=100, color='g',
-           label='median 1.0 km')
-# ax.scatter(Up, max500, marker='x', s=100, color='b',
-#            label='max 0.5 km')
-# ax.scatter(Up, max1000, marker='x', s=100, color='g',
-#            label='max 1.0 km')
-ax.set_xlim([minU, maxU])
-ax.set_ylim([minN, maxN])
-
-plt.legend(loc=2, scatterpoints=1)
-# plt.axhline(0.007, color='r', ls='--')
-# plt.axhline(0.025, color='r', ls='--')
-plt.xlabel('U [m s-1]')
-plt.ylabel('N [s-1]')
-plt.title('Storm 2 Froude number (Fr=U/NH)')
-plt.show(block=False)
+main()
